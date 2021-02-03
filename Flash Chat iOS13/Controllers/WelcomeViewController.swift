dd9ec4b65d2cd4398f2f5917cd298b32a9389988
Mark Thieu <41586405+markthieu@users.noreply.github.com>
2021-02-03 14:15:16 +0000

    Add files via upload

---
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -0,0 +1,31 @@
+//
+//  WelcomeViewController.swift
+//  Flash Chat iOS13
+//
+//  Created by Mark Thieu
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.isNavigationBarHidden = false
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        titleLabel.text = K.appName
+       
+    }
+    
+
+}
---
+++ Flash Chat iOS13/Models/Message.swift
@@ -0,0 +1,13 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Marmago on 17/11/2020.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String
+    let body: String
+}
